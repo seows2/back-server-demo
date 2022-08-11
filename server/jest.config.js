ae2b2f799a60349ad234971b7c11a2d05e9d3cf3
seows2 <seows2@naver.com>
2022-08-11 21:22:19 +0900

    [chore] jest 세팅

---
+++ server/jest.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).(ts|js)'],
+  transform: {
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coverageReporters: ['html', 'text'],
+  coverageDirectory: 'coverage',
+};
