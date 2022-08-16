d80ba1f6c26914db587e7b6831f0c61a7ecb2639
seows2 <seows2@naver.com>
2022-08-16 21:20:07 +0900

    [test] utils 함수들 테스트코드 작성

---
+++ server/src/test/utils/hash.test.ts
@@ -0,0 +1,56 @@
+import bcrypt from 'bcrypt';
+import * as hashHelper from '../../utils/hash';
+
+describe('hash utils 테스트', () => {
+  describe('generateHash() 함수 테스트', () => {
+    const mockPW = 'testPassword';
+
+    it('bcrypt를 사용해 암호화된 문자열이 반환되어야 합니다.', () => {
+      const hashedPw = hashHelper.generateHash(mockPW);
+      expect(hashedPw).toBeDefined();
+    });
+
+    it('암호화된 문자열과 암호화되지 않은 문자열을 bcrypt.compareSync로 비교했을 때 참이어야 합니다.', () => {
+      const hashedPw = hashHelper.generateHash(mockPW);
+      const isValid = bcrypt.compareSync(mockPW, hashedPw);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('암호화된 문자열과 원본과 다른 문자열을 bcrypt.compareSync로 비교했을 때 거짓이어야 합니다.', () => {
+      const hashedPw = hashHelper.generateHash(mockPW);
+      const isValid = bcrypt.compareSync(`DIFF_${mockPW}`, hashedPw);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('같은 문자열을 암호화해도 다른 결과물이 나와야 합니다.', () => {
+      const hashedPw1 = hashHelper.generateHash(mockPW);
+      const hashedPw2 = hashHelper.generateHash(mockPW);
+      expect(hashedPw1).not.toBe(hashedPw2);
+    });
+  });
+
+  describe('comparePassword() 함수 테스트', () => {
+    const mockPW = 'testPassword';
+
+    it('bcrypt를 사용해 암호화된 문자열과 원본 문자열을 비교했을 때 참을 반환해야 합니다.', () => {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPw = bcrypt.hashSync(mockPW, salt);
+      const isValid = hashHelper.comparePassword(hashedPw, mockPW);
+      expect(isValid).toBeTruthy();
+    });
+
+    it('bcrypt를 사용해 암호화된 문자열과 원본과 다른 문자열을 비교했을 때 거짓을 반환해야 합니다.', () => {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPw = bcrypt.hashSync(mockPW, salt);
+      const isValid = hashHelper.comparePassword(hashedPw, `DIFF_${mockPW}`);
+      expect(isValid).toBeFalsy();
+    });
+
+    it('bcrypt를 사용해 암호화된 문자열과 빈 문자열을 비교했을 때 거짓을 반환해야 합니다.', () => {
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPw = bcrypt.hashSync(mockPW, salt);
+      const isValid = hashHelper.comparePassword(hashedPw, '');
+      expect(isValid).toBeFalsy();
+    });
+  });
+});
---
+++ server/src/test/utils/jwt.test.ts
@@ -0,0 +1,32 @@
+import { getJwtAlgorithm } from '../../utils/jwt';
+import { Algorithm } from 'jsonwebtoken';
+
+describe('jwt utils 테스트', () => {
+  describe('getJwtAlgorithm() 함수 테스트', () => {
+    it('허용된 알고리즘의 경우 그대로 반환해야 합니다.', () => {
+      const jwtAlgorithms: Algorithm[] = [
+        'HS256',
+        'HS384',
+        'HS512',
+        'RS256',
+        'RS384',
+        'RS512',
+        'ES256',
+        'ES384',
+        'ES512',
+        'PS256',
+        'PS384',
+        'PS512',
+        'none',
+      ];
+      jwtAlgorithms.forEach(algo => {
+        expect(getJwtAlgorithm(algo)).toBe(algo);
+      });
+    });
+
+    it('허용되지 않은 알고리즘을 넘길 경우 none을 반환해야 합니다.', () => {
+      expect(getJwtAlgorithm('UNEXPECTED_ALGORITHM')).toBe('none');
+      expect(getJwtAlgorithm('SUPERFRAMEMASTER')).toBe('none');
+    });
+  });
+});
