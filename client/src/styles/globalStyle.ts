a7dce5be2ec1ee6556f6b79d49cc7fe2efa3078f
seows2 <seows2@naver.com>
2022-09-06 23:36:20 +0900

    [fix] globalStype warning 메세지 제거

--- client/src/styles/globalStyle.ts
+++ client/src/styles/globalStyle.ts
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
-
     ${reset}
     #root {
         width: 100%;
